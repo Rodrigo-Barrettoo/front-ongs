91f6650e3c672e2849fd8590366e3c40998fc3cc
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2020-12-21 20:24:35 -0300

    configurando o styld global.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
-const App: React.FC = () => {
-  return <h1>Hell word</h1>;
-};
+const App: React.FC = () => (
+  <>
+    <h1>Hell word</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
